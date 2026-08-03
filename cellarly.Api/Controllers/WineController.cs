35498bfc7223890b06db634ffd0e69bfe1d410f6
Mariana <marianalopes697@gmail.com>
2026-08-03 18:00:09 -0300

    cria o model wine

---
+++ cellarly.Api/Controllers/WineController.cs
@@ -0,0 +1,20 @@
+﻿using cellarly.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace cellarly.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WineController : ControllerBase
+    {
+        public List<Wine> Wines = new List<Wine>();
+
+        [HttpPost]
+        public void CreateWine([FromBody] Wine wine)
+        {
+            Wines.Add(wine);
+        }
+    }
+}
