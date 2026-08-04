2052a7f48652e891b24699a4d7ded53b25519b06
Mariana <marianalopes697@gmail.com>
2026-08-04 20:56:39 -0300

    chore: ignore appsettings.Development.json

---
+++ cellarly.Api/Data/WineContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using cellarly.Api.Models;
+
+namespace cellarly.Api.Data
+{
+    public class WineContext : DbContext
+    {
+        public WineContext(DbContextOptions<WineContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Wine> Wines { get; set; } = null!;
+    }
+}
